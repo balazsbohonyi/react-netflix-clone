ef4b30d632e1aed15ff613ccac4364ac0e0c3fd4
Balazs Bohonyi <balazs.bohonyi@gmail.com>
2020-11-06 21:20:31 +0200

    fixed the selectio filter for the movies / added support for the browse page cards / added support for playing videos

--- src/utils/selection-filter.js
+++ src/utils/selection-filter.js
@@ -3,7 +3,7 @@ export default function selectionFilter({ series, films }) {
     series: [
       {
         title: 'Documentaries',
-        data: series.filter((item) => item.genre === 'dcoumentaries'),
+        data: series.filter((item) => item.genre === 'documentaries'),
       },
       {
         title: 'Comedies',
@@ -14,6 +14,10 @@ export default function selectionFilter({ series, films }) {
         data: series.filter((item) => item.genre === 'children'),
       },
       {
+        title: 'Crime',
+        data: series.filter((item) => item.genre === 'crime'),
+      },
+      {
         title: 'Feel Good',
         data: series.filter((item) => item.genre === 'feel-good'),
       },
@@ -21,23 +25,23 @@ export default function selectionFilter({ series, films }) {
     films: [
       {
         title: 'Drama',
-        data: series.filter((item) => item.genre === 'drama'),
+        data: films.filter((item) => item.genre === 'drama'),
       },
       {
         title: 'Thriller',
-        data: series.filter((item) => item.genre === 'thriller'),
+        data: films.filter((item) => item.genre === 'thriller'),
       },
       {
         title: 'Children',
-        data: series.filter((item) => item.genre === 'children'),
+        data: films.filter((item) => item.genre === 'children'),
       },
       {
         title: 'Suspense',
-        data: series.filter((item) => item.genre === 'suspense'),
+        data: films.filter((item) => item.genre === 'suspense'),
       },
       {
         title: 'Romance',
-        data: series.filter((item) => item.genre === 'romance'),
+        data: films.filter((item) => item.genre === 'romance'),
       },
     ],
   };
